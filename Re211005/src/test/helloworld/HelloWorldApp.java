592352e2f2359ffcda5678883a88915e789eea97
jenniferyjk <jaysstar0310@gmail.com>
2021-10-15 19:16:22 +0900

    first commit

---
+++ Re211005/src/test/helloworld/HelloWorldApp.java
@@ -0,0 +1,8 @@
+package test.helloworld;
+
+public class HelloWorldApp {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+} // 컴퓨터는 자바의 main 메서드 안의 내용을 인식해 실행한다
+// 복습 시작
